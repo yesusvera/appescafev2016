0522cffe8ade045f63c97887e4e35ef1a9343f65
jonathan.carrillo <jj.salcedo@hotmail.com>
2016-02-27 15:58:27 -0300

    Conclusão do formulario Piticaia e Branco

--- app/src/main/java/br/org/unesco/appesca/util/ConstantesIdsFormularios.java
+++ app/src/main/java/br/org/unesco/appesca/util/ConstantesIdsFormularios.java
@@ -87,9 +87,9 @@ public class ConstantesIdsFormularios {
 
               R.id.b5_q1, R.id.b5_q2, R.id.b5_q3,R.id.b5_q4,R.id.b5_q5,
 
-              R.id.b6_q1, R.id.b6_q2, R.id.b6_q3, R.id.b6_q4, R.id.b6_q5, R.id.b6_q6, R.id.b6_q7, R.id.b6_q8,
+              R.id.b6_q1, R.id.b6_q2, R.id.b6_q3, R.id.b6_q4, R.id.b6_q5, R.id.b6_q6,
 
-              R.id.b7_q1, R.id.b7_q2, R.id.b7_q3,R.id.b7_q4,R.id.b7_q5 ,R.id.b7_q6,R.id.b7_q7, R.id.b7_q8,R.id.b7_q9,R.id.b7_q10,R.id.b7_q11,R.id.b7_q12,R.id.b7_q13,
+              R.id.b7_q1, R.id.b7_q2,
 
               R.id.b8_q1, R.id.b8_q2, R.id.b8_q3,R.id.b8_q4,R.id.b8_q5,R.id.b8_q6,R.id.b8_q7,
 
@@ -121,7 +121,15 @@ public class ConstantesIdsFormularios {
             R.layout.fcmr_reg_b5_q1, R.layout.fcmr_reg_b5_q2, R.layout.fcmr_reg_b5_q3,
             R.layout.fcmr_reg_b5_q4, R.layout.fcmr_reg_b5_q5,
 
-            R.layout.fcmr_pb_b6_q1, R.layout.fcmr_pb_b6_q2
+            R.layout.fcmr_pb_b6_q1, R.layout.fcmr_pb_b6_q2, R.layout.fcmr_pb_b6_q3,
+            R.layout.fcmr_pb_b6_q4, R.layout.fcmr_pb_b6_q5, R.layout.fcmr_pb_b6_q6,
+
+            R.layout.fcmr_pb_b7_q1, R.layout.fcmr_pb_b7_q2,
+
+            R.layout.fcmr_pb_b8_q1, R.layout.fcmr_pb_b8_q2, R.layout.fcmr_pb_b8_q3, R.layout.fcmr_pb_b8_q4,
+              R.layout.fcmr_pb_b8_q5, R.layout.fcmr_pb_b8_q6,
+
+            R.layout.fcmr_pb_b9_q1
 
         };
 
