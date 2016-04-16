7d57b720518fcbce7bc215c6966187111988c3f1
yesus <yesusvera@gmail.com>
2016-04-16 15:36:24 -0300

    New changelist

--- app/src/main/java/br/org/unesco/appesca/util/ConstantesREST.java
+++ app/src/main/java/br/org/unesco/appesca/util/ConstantesREST.java
@@ -4,19 +4,20 @@ package br.org.unesco.appesca.util;
  * Created by yesus on 1/11/16.
  */
 public class ConstantesREST {
-    public static String HOST = "http://10.0.0.108:8080/appesca-web/";
-//    public static String HOST = "http://sauber03.unesco.org.br/appesca-web/";
 
-    //private final static String HOST = "http://macbookyesus.local:8080/appesca-web/";
+//     public static String HOST = "http://192.168.0.103:8080/appesca-web/";
+         public static String HOST = "http://sauber03.unesco.org.br/appesca-web/";
+     //private final static String HOST = "http://macbookyesus.local:8080/appesca-web/";
 
-    /**
+     /**
      * SERVICOS REST
      */
     public final static String AUTHENTICATION_SERVICE = "rest/usuario/autenticacao";
-    public final static String USUARIO_IMAGEM = "rest/usuario/imagem";
+    public final static String USUARIO_IMAGEM = "rest/usuario/imag  em";
     public final static String FORMULARIO_LISTA = "rest/formulario/lista";
+    public final static String FORMULARIO_INSERIR = "rest/formulario/inserir";
+    public final static String FORMULARIO_INSERIR_VS2 = "InsertFormulario"; //Inserido para não causar incompatibilidade com as versões anteriores que já estão em prod.
     public final static String IMAGEM_USUARIO = "rest/usuario/imagem";
-
 
     public static String getURLService(String SERVICE_REST){
         return HOST + SERVICE_REST;
